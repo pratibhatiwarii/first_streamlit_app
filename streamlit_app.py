680d0674b88ec2db4c9a68623b45545b81673dde
pratibhatiwarii <77544117+pratibhatiwarii@users.noreply.github.com>
2023-01-05 01:28:35 -0500

    Update streamlit_app.py
    updated menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
+
 streamlit.title('My Parents new healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega3 and Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
